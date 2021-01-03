110d1780af04c544ecce7a4f9064fe8875bfaed1
benjaminwan <benjaminwan@163.com>
2021-01-03 16:06:53 +0800

    修改项目名称，升级Jni，增加输入参数，修改inti方法为setNumThread

--- run-test/run-test-java.cmd
+++ run-test/run-test-java.cmd
@@ -5,7 +5,27 @@ chcp 65001
 echo "Setting the Number of Threads=%NUMBER_OF_PROCESSORS% Using an OpenMP Environment Variable"
 set OMP_NUM_THREADS=%NUMBER_OF_PROCESSORS%
 
-:: run Win x64
-java -Djava.library.path=. -Dfile.encoding=UTF-8 -jar OcrLiteJvm.jar models test_imgs/1.jpg
+:: run Windows
+java -Djava.library.path=. -Dfile.encoding=UTF-8 -jar OcrLiteJvm.jar models test_imgs/1.jpg ^
+4 ^
+50 ^
+0 ^
+0.6 ^
+0.3 ^
+3 ^
+2.0 ^
+1 ^
+1
+
+:: numThread
+:: padding
+:: imgResize
+:: boxScoreThresh
+:: boxThresh
+:: minArea
+:: unClipRatio
+:: doAngle
+:: mostAngle
+
 PAUSE
 @ENDLOCAL
